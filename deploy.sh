93ac1ac9c7d0a62847bbc8fd81fb4f3f56abf093
rookieInTraining <rookieInTraining@users.noreply.github.com>
2020-02-18 12:43:56 +0530

    Debugging travis failure

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,28 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-cd public
-
 if [ -n "$GITHUB_AUTH_SECRET" ]
 then
-    touch ~/.git-credentials
-    chmod 0600 ~/.git-credentials
-    echo $GITHUB_AUTH_SECRET > ~/.git-credentials    
-    
-    git config credential.helper store
-    git config user.email "rookieInTraining@users.noreply.github.com"
-    git config user.name "rookieInTraining"
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
+
+    git config --global credential.helper store
+    git config --global user.email "rookie-in-training-bot@users.noreply.github.com"
+    git config --global user.name "rookie-in-training-bot"
+    git config --global push.default simple
 fi
 
-git add .
-git commit -m "Rebuild site"
+rm -rf deployment
+git clone -b master https://github.com/rookieInTraining/rookieintraining.github.io.git deployment
+rsync -av --delete --exclude ".git" public/ deployment
+cd deployment
+
+git add -A
+
+# we need the || true, as sometimes you do not have any content changes
+# and git woundn't commit and you don't want to break the CI because of that
+git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
+
 git push --force origin HEAD:master
+
+cd ..
+rm -rf deployment
