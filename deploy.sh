9dbd7012275f93709bb6749966852d3757a29af4
rookieInTraining <rookieInTraining@github.io>
2025-05-01 22:07:41 +0530

    fix for ssh and building the site

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-mkdir -p ~/.ssh
-echo $DEPLOY_KEY > ~/.ssh/deploy-key   # Save the private key to a file
-chmod 600 ~/.ssh/deploy-key 
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 git config --global user.email "rookie-in-training-bot@users.noreply.github.com"
 git config --global user.name "rookie-in-training-bot"
 git config --global push.default simple
-
-GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy-key -o IdentitiesOnly=yes'
 
 rm -rf deployment
 git clone -b master https://rookie-in-training-bot:$DEPLOY_KEY@github.com/rookieintraining/rookieintraining.github.io.git deployment
